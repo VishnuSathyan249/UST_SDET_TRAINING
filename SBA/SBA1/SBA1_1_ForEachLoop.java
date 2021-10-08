7002b9a879aabc8504e8e9d4eb301ec4e286eaae
VishnuSathyan249 <92089638+VishnuSathyan249@users.noreply.github.com>
2021-10-08 11:53:11 +0530

    Update SBA1_1_ForEachLoop.java

--- SBA/SBA1/SBA1_1_ForEachLoop.java
+++ SBA/SBA1/SBA1_1_ForEachLoop.java
@@ -5,8 +5,13 @@ public class SBA1_1_ForEachLoop {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int[] array = new int[]{10,2,3,45,67,11,22,67,89,23,};
+		System.out.println("The array of elements are : ");
 		
 		for(int num : array)
-			System.out.println(num);
+			System.out.print(" " +num );
 	}
 }
+
+output
+The array of elements are : 
+ 10 2 3 45 67 11 22 67 89 23
