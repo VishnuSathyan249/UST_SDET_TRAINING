1dd29396f52bce02d204b5ca11c83aae2fc47ef7
VishnuSathyan249 <92089638+VishnuSathyan249@users.noreply.github.com>
2021-10-14 15:46:58 +0530

    Add files via upload

---
+++ SBA/SBA1/SBA1_5_StringReverse.java
@@ -0,0 +1,39 @@
+package sample;
+
+import java.util.Scanner;
+
+public class SBA1_5_StringReverse {
+	
+	
+	public static void main(String args[]) {
+	
+		char a[] = new char[20];
+		String str;
+		
+		
+		System.out.println("Enter the string");
+		Scanner sc = new Scanner(System.in);	
+		str = sc.next();
+		a = str.toCharArray();
+		
+		StringBuilder str1 = new StringBuilder(str);
+		
+		System.out.println();
+		System.out.println("String before reverse");
+		System.out.println(str);
+		
+
+		
+		System.out.println();
+		System.out.println("String after reverse using inbuilt function");
+		System.out.println(str1.reverse());
+		
+		System.out.println();
+		System.out.println("String after reverse using char array");
+		
+		for (int i = str.length()-1; i>=0 ; i--) 
+		{
+				System.out.print(a[i]);
+		}
+}
+}
