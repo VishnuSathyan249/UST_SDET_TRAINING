cfc13b77ef2e3a46b8bf600eafcb7666c7763537
VishnuSathyan249 <92089638+VishnuSathyan249@users.noreply.github.com>
2021-10-22 21:50:49 +0530

    Add files via upload

---
+++ TESTING TOOLS/CYPRESS/Exp1.js
@@ -0,0 +1,18 @@
+// test suite name
+describe('simple Test', function () {
+    // Test case
+       it('Scenario 1', function (){
+          // test step for URL launching
+          cy.visit("https://www.google.com/");
+       });
+    });
+ 
+// test suite name
+describe('simple Test', function () {
+   // Test case
+      it('Scenario 1', function (){
+         // test step for URL launching
+         cy.visit("https://www.google1.com/");
+      });
+   });
+
