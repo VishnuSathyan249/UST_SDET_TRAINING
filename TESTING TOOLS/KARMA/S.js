0adddc660daa0775cf457623ba46270450058a15
VishnuSathyan249 <92089638+VishnuSathyan249@users.noreply.github.com>
2021-10-22 21:53:58 +0530

    Add files via upload

---
+++ TESTING TOOLS/KARMA/S.js
@@ -0,0 +1,11 @@
+describe('Sample test', function() {
+    it('Condition is true', function() {
+      expect('AnguarJS').toBe('AngularJS');
+    });
+
+    it('Condition is true', function() {
+      expect('AngularJS').toBe('AngularJS');
+    });
+
+  });
+  
